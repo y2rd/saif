3b3dc9c166919676a2f31f65456afec7ceea2df5
haider <haider@store.local>
2026-09-19 10:04:00 +0300

    Updates

--- android/app/src/main/java/com/haider/mystore/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.haider.mystore;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- undo.cjs
+++
@@ -1,3 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('test1.jsx', 'utf8');
-fs.writeFileSync('C:\\Users\\johar\\Documents\\haider\\my-store\\src\\AdminDashboard.jsx', code);
